c79450514a8510eef262a407812db94981605118
Aakash Goel <goelaakash79@gmail.com>
2019-02-26 18:13:30 +0530

    db connected

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const index = require('./index');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
+mongoose.Promise = global.Promise;
+// Mongoose Connect
+const connectionString = "mongodb://ritwick80:ritwick123@cluster0-shard-00-00-rloq9.mongodb.net:27017,cluster0-shard-00-01-rloq9.mongodb.net:27017,cluster0-shard-00-02-rloq9.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true";
+
+mongoose.connect(connectionString, { useNewUrlParser: true })
+    .then(function() {
+        console.log("MongoDB Connected");
+    })
+    .catch(function(err) {
+        console.log(err);
+});
+
 app.use(bodyParser.urlencoded({ extended: true }));
-const PORT = 5000;
+const PORT = 3000;
 
 app.set("view engine", "ejs");
 
@@ -17,7 +30,8 @@ app.get("/index", (req, res) => {
 });
 
 app.post("/form-submission", (req, res) => {
-	res.send("hello" + req.body.firstname);
+	console.log(req.body);
+	res.send("hello " + req.body.firstname);
 	// console.log(req.body.firstname);	
 });
 
